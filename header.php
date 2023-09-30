8afe1513610d866318699b4c186f7a87c56950d6
AchiniJayathilake <achini.s.jayathilake@gmail.com>
2023-09-30 15:20:04 +0530

    header

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid  justify-content-center">
+        <div class="row">
+            <div class="col-12">
+                <div class="row  text-white fw-semibold" style="background-color: #27AE60;">
+                    <div class="col-5 py-2">Icon</div>
+                    <div class="col-1 py-2 text-center">Home</div>
+                    <div class="col-1 py-2 text-center">About</div>
+                    <div class="col-1 py-2 text-center">Store</div>
+                    <div class="col-1 py-2 text-center">Register</div>
+                    <div class="col-1 py-2 text-center ">Cart</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="  d-flex container-fluid justify-content-center vh-100">
+
+    <div class="row ">
+        
+    </div>
+
+    </div>
+    
+</body>
+</html>
