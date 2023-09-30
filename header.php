b5149abfc7b8aa195dea094324485bffa0a706df
AchiniJayathilake <achini.s.jayathilake@gmail.com>
2023-09-30 16:02:57 +0530

    header

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +8,13 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <title>Document</title>
 </head>
+
 <body>
     <div class="container-fluid  justify-content-center">
         <div class="row">
             <div class="col-12">
-                <div class="row  fw-semibold bg-info align-items-center" >
+
+                <div class="row  fw-semibold bg-info align-items-center">
                     <!-- style="background-color: #27AE60;" -->
                     <div class="col-5 py-2">
                         <img src="resources/logo.png" style="width: 50px;" alt="">
@@ -19,12 +22,18 @@
                     <div class="col-1 py-2 text-center">Home</div>
                     <div class="col-1 py-2 text-center">About</div>
                     <div class="col-1 py-2 text-center">Store</div>
-                    <div class="col-1 py-2 text-center" onclick="">Register</div>
+                    <div class="col-1 py-2 text-center">
+                    <span class="curser" onclick="showLoginModel();">Register</span>
+                    </div>
                     <div class="col-1 py-2 text-center ">Cart</div>
                 </div>
+
+                <div class="modal-dialog modal-dialog-centered" id="loginModel"></div>
             </div>
         </div>
     </div>
-    
+
+    <script src="script.js"></script>
 </body>
+
 </html>
---
+++ script.js
@@ -0,0 +1,8 @@
+var bModel
+function showLoginModel(){
+var model = document.getElementById("loginModel");
+ bModel= new bootstrap.Model(model);
+ bModel.show();
+
+
+}
