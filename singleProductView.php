2e591e22d3b96765ceec921873f3d87948e00fa1
AchiniJayathilake <achini.s.jayathilake@gmail.com>
2023-10-02 14:03:57 +0530

    single product View design

--- singleProductView.php
+++ singleProductView.php
@@ -19,9 +19,69 @@
 
             <div class="col-12">
                 <div class="row justify-content-center">
-                    <div class="col-12 py-3">
+
+                    <div class="col-12 py-3 mt-3">
                         <span class="fs-5 fw-semibold">Single Product View</span>
                     </div>
+
+
+                    <hr>
+
+                    <div class="col-11 py-3">
+                        <div class="row justify-content-center gap-2">
+                            <div class="col-4">
+                                <img src="resources/broccali.png" class="border border-1" style="width: 300px;" alt="">
+                            </div>
+                            <div class="col-6">
+                                <div class="row">
+
+                                    <div class="col-12">
+                                        <span class="fw-semibold fs-5">BROCCOLI - (100g)</span>
+                                    </div>
+
+                                    
+
+                                    <div class="col-12 mt-5">
+                                        <span class="text-danger fs-5">Price : Rs.<span class="text-danger fs-5">190</span>.00 </span>
+
+                                        &nbsp;
+                                        <span class=" fs-6  text-decoration-line-through ">Rs. 197.5 .00</span>
+                                        &nbsp;
+                                        <span class=" fs-6 text-black-50">Save Rs.7.5 .00 (5%)</span>
+                                    </div>
+
+                                    <div class="col-12 mt-3">
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <input type="number" class="form-control  form-control-sm" value="1">
+                                            </div>
+                                            <div class="col-9"><span>Only 8 items left</span></div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-12 mt-4">
+                                        <div class="row g-2">
+                                            <div class="col-4 d-grid">
+                                                <button class="btn btn-success btn-sm fw-semibold">Buy Now</button>
+                                            </div>
+                                            <div class="col-4 d-grid">
+                                                <button class="btn btn-warning btn-sm fw-semibold">Add To Cart &nbsp; <i class="bi bi-cart-plus fs-5"></i></button>
+                                            </div>
+                                            <div class="col-4 d-grid">
+                                                <button class="btn btn-sm fw-semibold" style="background-color: #c3c3c3;">Add To Wishlist &nbsp; <i class="bi bi-heart-fill text-danger "></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+
+
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
