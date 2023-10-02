0ce9321ae59465c9e087c13edd41679a5b6f71fa
AchiniJayathilake <achini.s.jayathilake@gmail.com>
2023-10-02 14:24:49 +0530

    user Profile blank for sndu

---
+++ userProfile.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Aswenna | User Profile</title>
+    <link rel="icon" href="resources/icon/icon.png">
+
+</head>
+
+<body>
+    <div class="container-fluid justify-content-center vh-100">
+
+        <div class="row justify-content-center">
+            <?php include "header.php"; ?>
+
+           
+
+            <?php include "footer.php"; ?>
+        </div>
+
+    </div>
+    <script src="js/bootstrap.bundle.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
